1c38c72f368d2c86881f929ca4a249095023cfc5
geonmo.nine <geonmo.nine@navercorp.com>
2024-04-23 15:48:38 +0900

    commithelper 생성

---
+++ packages/commithelper/README.md
@@ -0,0 +1,87 @@
+# @naverpay/commithelper
+
+[husky](https://typicode.github.io/husky/) 의 `commit-msg`훅을 기반으로, 커밋에 필요한 각 종 도구를 제공하는 cli 입니다.
+
+## How to use
+
+### .husky/commit-msg
+
+```
+npx --yes @naverpay/commithelper@latest $1
+```
+
+> `@latest`가 아니어도 상관없습니다만, `latest`라면 항상 최신버전을 사용하실 수 있습니다.
+
+## what it does
+
+### 커밋에 이슈 태깅
+
+```shell
+➜  your-repo git:(feature/1) git add . &&  git commit -m ":memo: test"
+ℹ No staged files match any configured task.
+$ git branch --show-current
+feature/1
+[feature/1 1e70c244f] [#1] :memo: test
+ 1 file changed, 1 insertion(+)
+```
+
+이제 커밋을 하면, `.commithelperrc.json`에 따라서 자동으로 이슈번호가 태깅됩니다.
+
+#### 사용자화
+
+만약 사전에 정의된 규칙외에 레포에 특별한 규칙을 넣고 싶으시다면, [cosmicconfig](https://github.com/cosmiconfig/cosmiconfig) 규칙을 `commithelper`로 사용해주세요. `cosmicconfig`는 `eslint` `prettier` 등지에서 널리 사용되는 `rc` 설정을 읽어서 사용하는 라이브러리로, `commithelper`도 `cosmicconfig`를 사용합니다. 그리고 사전에 정의된 규칙과 충돌이 있다면 `cosmicconfig`를 우선합니다.
+
+#### `.commithelperrc.json`
+
+```json
+{
+    "rules": {
+        "feature": null,
+        "qa": "your-org/your-repo"
+    }
+}
+```
+
+> 위와 같이 있다면,
+>
+> - `feature/1`은 `[#1]` 로 태깅됩니다.
+> - `qa/1`은 `[your-org/your-repo#1]`로 태깅됩니다.
+
+### 커밋 방지
+
+- `main`, `develop` `master` 브랜치에 직접적으로 하는 것을 방지하며, `main`, `develop` `master` 브랜치에 커밋을 하려고 하면 에러를 발생시킵니다.
+- 이외에 다른 브랜치를 막고 싶다면, `commithelperrc` 의 `protect`필드를 추가해주세요.
+
+### commithelperrc
+
+`.commithelperrc`의 규칙은 다음과 같습니다.
+
+```json
+{
+    "protect": ["main", "master", "develop"],
+    "rules": {
+        "feature": null,
+        "qa": "your-org/your-repo"
+    }
+}
+```
+
+#### rules
+
+- 키는 브랜치명 규칙입니다. `feature`를 키로 한다면, `feature/***` 네이밍의 브랜치들이 타겟이 됩니다.
+- 값은 해당 브랜치와 매칭할 레포명입니다. 예를 들어 'your-org/your-repo' 라고 값이 설정되어 있다면, 'your-org/your-repo#1' 로 연결됩니다.
+- 값이 만약 `null`이라면, 자기 저장소 자신을 태깅합니다. `feature`가 `null`이라면, 위와 같이 `[#1]`로 태깅합니다.
+
+#### protect
+
+- 커밋을 막고 싶은 브랜치를 배열로 넣어주세요. `main`, `master`, `develop`는 기본적으로 막힙니다.
+
+## Q&A
+
+- 커밋메시지에 이미 `[your-org/your-repo#1]` 와 같은 형식의 태깅이 되어 있으면 어떻게 되나요?
+  - `commithelper`는 동작하지 않고, 해당 커밋 메시지 태깅을 존중합니다.
+- `.commithelperrc` 룰과 자체 규칙 룰이 충돌하면 어떻게 되나요?
+  - `cosmicconfig`를 우선합니다.
+- `feature/1_xxx` `feature/1-xxx` 와 같은 형태의 브랜치명은 어떻게 처리되나요?
+  - `feature/1`로 처리됩니다.
+  - 자세한 내용은 테스트 코드를 참조하세요.
---
+++ packages/commithelper/bin/cli.ts
@@ -0,0 +1,165 @@
+import {exec} from 'child_process'
+import fs from 'fs/promises'
+import {join} from 'path'
+import process from 'process'
+
+import {cosmiconfigSync} from 'cosmiconfig'
+import meow from 'meow'
+
+import {
+    ISSUE_TAGGING_MAP,
+    BRANCH_ISSUE_TAGGING_REGEX,
+    PURE_BRANCH_ISSUE_TAGGING_REGEX,
+    DEFAULT_PROTECTED_BRANCHES,
+    ISSUE_TAGGING_REGEX,
+} from '../src/constant.js'
+
+async function getCurrentBranchName(): Promise<string> {
+    return new Promise((resolve, reject) => {
+        exec('git branch --show-current', (err, stdout, stderr) => {
+            if (err) {
+                reject(err)
+                return
+            }
+            if (stderr) {
+                reject(new Error(stderr))
+                return
+            }
+            resolve(stdout.trim() as string)
+        })
+    })
+}
+
+export function getCommitMessageByBranchName(
+    branchName: string,
+    originCommitMessage: string,
+    externalConfig?: Record<string, string | null>,
+) {
+    let finalCommitMessage = ''
+
+    // 커밋 메시지 내부에 이슈 태깅이 되어 있을 경우, 브랜치명을 확인하지 않고 그냥 보낸다.
+    if (ISSUE_TAGGING_REGEX.test(originCommitMessage)) {
+        finalCommitMessage = originCommitMessage
+    }
+    // 현재 브랜치명에서 이슈 태깅을 찾아서 커밋 메시지에 추가한다.
+    else {
+        const foundedIssueTagging = branchName.match(BRANCH_ISSUE_TAGGING_REGEX)
+
+        // 브랜치 명이 정규식과 일치한다면
+        if (foundedIssueTagging && foundedIssueTagging.length > 0) {
+            // 브랜치명에는 _fx 와 같은 추가정보가 있을 수 있으므로, 서비스 명을 추가로 뽑아낸다.
+            const foundBranch = foundedIssueTagging[0].match(PURE_BRANCH_ISSUE_TAGGING_REGEX)
+
+            if (!foundBranch) {
+                // rebase 등으로 브랜치 없이 작업할 수 있음
+                return originCommitMessage
+            }
+
+            const branchInfo = foundBranch[0]
+
+            const serviceName = branchInfo.split('/')[0].toLowerCase()
+            const issueNumber = branchInfo.split('/')[1]
+
+            /**
+             * @description 내부 상수를 후순위. 사용자 설정이 덮어쓴다.
+             */
+            const issueMap = {
+                ...ISSUE_TAGGING_MAP,
+                ...externalConfig,
+            } as Record<string, string | null>
+
+            // 태깅 맵 객체에 맞는게 있는지 확인한다.
+            const repoName = issueMap[serviceName]
+
+            // 사용자 설정에 있거나 내부상수에 정의된 경우
+            if (repoName) {
+                finalCommitMessage = `[${repoName}#${issueNumber}] ${originCommitMessage}`
+            }
+            // null 로 명시되었다는 것은 자기자신 (#123) 으로 태깅하겠다는 뜻
+            else if (repoName === null) {
+                finalCommitMessage = `[#${issueNumber}] ${originCommitMessage}`
+            }
+            // undefined 는 못찾았다는 뜻. 커밋메시지를 그냥 돌려보낸다.
+            else {
+                finalCommitMessage = originCommitMessage
+            }
+        }
+        // 맞는게 없다면 그냥 기존 메시지로 보낸다
+        else {
+            finalCommitMessage = originCommitMessage
+        }
+    }
+    return finalCommitMessage
+}
+
+function readExternalConfig(): {rules: Record<string, string | null>; protect: string[]} {
+    const explorerSync = cosmiconfigSync('commithelper')
+
+    const searchedFor = explorerSync.search()
+
+    if (!searchedFor) {
+        return {
+            rules: {},
+            protect: [],
+        }
+    }
+
+    return searchedFor.config
+}
+
+export function isStringMatchingPatterns(patternsArray: string[], stringToCheck: string) {
+    const patterns = [...DEFAULT_PROTECTED_BRANCHES, ...patternsArray]
+
+    return patterns.some((pattern) => {
+        if (pattern.endsWith('/*')) {
+            const basePattern = pattern.slice(0, -1)
+            const regex = new RegExp(`^${basePattern}.+$`)
+            return regex.test(stringToCheck)
+        } else {
+            return pattern === stringToCheck
+        }
+    })
+}
+
+export async function run() {
+    const cli = meow(`Tag issues to commit messages based on your current branch names.`, {
+        importMeta: import.meta,
+        flags: {help: {type: 'boolean', shortFlag: 'h'}, show: {type: 'boolean', shortFlag: 's'}},
+    })
+
+    const currentBranchName = (await getCurrentBranchName()).toLowerCase()
+
+    const {rules = {}, protect = []} = readExternalConfig()
+
+    if (cli.flags.show) {
+        /**
+         * @description 내부 상수를 후순위. 사용자 설정이 덮어쓴다.
+         */
+        const issueMap = {
+            ...ISSUE_TAGGING_MAP,
+            ...rules,
+        } as Record<string, string | null>
+
+        // eslint-disable-next-line no-console
+        console.log(JSON.stringify(issueMap, null, 2))
+        return
+    }
+
+    const commitMessagePath = cli.input[0]
+    const commitFilePath = join(process.cwd(), commitMessagePath)
+    const commitMessage = await fs.readFile(commitFilePath, 'utf8')
+
+    if (!commitMessage) {
+        throw new Error('Commit message is required.')
+    }
+
+    const isProtectedBranch = isStringMatchingPatterns(protect, currentBranchName)
+
+    if (isProtectedBranch) {
+        throw new Error(`You can't commit on this branch: ${currentBranchName}`)
+    }
+
+    const result = getCommitMessageByBranchName(currentBranchName, commitMessage, rules)
+
+    await fs.writeFile(commitFilePath, result, {encoding: 'utf8'})
+}
