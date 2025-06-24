28cfa5c17e0c17662d5870c352fe2aebe1bf9285
yujeong-jeon <yujeong.jeon@navercorp.com>
2025-06-25 00:46:25 +0900

    Edit bench.sh

--- bench.sh
+++ bench.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo "== 기존 패키지 실행 시간 측정 =="
+echo "== @naverpay/commit-helper =="
 for i in {1..5}; do
   time npx -y @naverpay/commit-helper .git/COMMIT_EDITMSG
 done
 
-echo "== 내가 작성한 패키지 실행 시간 측정 =="
+echo "== @naverpay/commithelper-go =="
 for i in {1..5}; do
   time npx commithelper-go .git/COMMIT_EDITMSG
 done
